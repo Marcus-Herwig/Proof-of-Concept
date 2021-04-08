03872d2c78d5bc840ae2a05b0b5f1f55fa669f96
Marcus-Herwig <70297887+Marcus-Herwig@users.noreply.github.com>
2021-04-08 15:20:32 -0500

    Add project files.

---
+++ Proof of Concept/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Proof_of_Concept
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WriteButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.dataDisplay = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataInput = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.addDataButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.processButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // WriteButton
+            // 
+            this.WriteButton.Location = new System.Drawing.Point(352, 205);
+            this.WriteButton.Name = "WriteButton";
+            this.WriteButton.Size = new System.Drawing.Size(204, 80);
+            this.WriteButton.TabIndex = 0;
+            this.WriteButton.Text = "Load csv";
+            this.WriteButton.UseVisualStyleBackColor = true;
+            this.WriteButton.Click += new System.EventHandler(this.WriteButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(352, 752);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(204, 75);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // dataDisplay
+            // 
+            this.dataDisplay.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataDisplay.Location = new System.Drawing.Point(95, 306);
+            this.dataDisplay.Name = "dataDisplay";
+            this.dataDisplay.ReadOnly = true;
+            this.dataDisplay.Size = new System.Drawing.Size(779, 422);
+            this.dataDisplay.TabIndex = 3;
+            this.dataDisplay.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(127, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1136, 46);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Proof of Concept: Reading from CSV file and writing to TXT file";
+            // 
+            // dataInput
+            // 
+            this.dataInput.Location = new System.Drawing.Point(966, 478);
+            this.dataInput.Name = "dataInput";
+            this.dataInput.Size = new System.Drawing.Size(559, 96);
+            this.dataInput.TabIndex = 5;
+            this.dataInput.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1005, 416);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(468, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Type the data that should be added ";
+            // 
+            // addDataButton
+            // 
+            this.addDataButton.Location = new System.Drawing.Point(1138, 619);
+            this.addDataButton.Name = "addDataButton";
+            this.addDataButton.Size = new System.Drawing.Size(226, 76);
+            this.addDataButton.TabIndex = 7;
+            this.addDataButton.Text = "Add data";
+            this.addDataButton.UseVisualStyleBackColor = true;
+            this.addDataButton.Click += new System.EventHandler(this.addDataButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(241, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(485, 32);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Please choose a .csv file to read from";
+            // 
+            // processButton
+            // 
+            this.processButton.Location = new System.Drawing.Point(705, 205);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(183, 80);
+            this.processButton.TabIndex = 9;
+            this.processButton.Text = "Process";
+            this.processButton.UseVisualStyleBackColor = true;
+            this.processButton.Click += new System.EventHandler(this.processButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1592, 925);
+            this.Controls.Add(this.processButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.addDataButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataDisplay);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.WriteButton);
+            this.Name = "Form1";
+            this.Text = "Proof of Concept ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button WriteButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.RichTextBox dataDisplay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox dataInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button addDataButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button processButton;
+    }
+}
+
---
+++ Proof of Concept/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LumenWorks.Framework.IO.Csv;
+using System.Windows.Forms;
+using System.IO;
+
+using System.Text.RegularExpressions;
+// Marcus Herwig, Austin Williams, Lauren Parsons, Martin Spinelli
+// Program to read from a csv file and then save to a new text file, and allow the user to enter more data if needed.
+// NOTE: this program is meant to be used to read department schedule files written in csv format.
+// March 27, 2021
+
+namespace Proof_of_Concept
+{
+   
+
+    public partial class Form1 : Form
+
+    {
+        string contents = " ";
+        string[] lines;
+        int count = 0;
+        OpenFileDialog file = new OpenFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void WriteButton_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog file = new OpenFileDialog(); // opens the file explorer so the user can choose what file to read from
+            file.ShowDialog(); // shows the file dialog ...
+            lines = System.IO.File.ReadAllLines(file.FileName); // creates a string array of all the classes in the schedule so each class is an index in an array
+            lines = lines.Skip(1).ToArray(); // skips the first line of schedule because it is not needed for this
+            for (int i = 0; i < lines.Length; i++) // sets up a loop that will put the contents of the string array into one string so we can display it
+            {
+                contents = contents + lines[i]; //adds each index of the string array that contains the data from the file 
+                contents = contents + "\n\n"; // adds a blank space between each index so it is easier to read
+                count = count + 1;
+            }
+            // displays the data that was written from the file
+            string CleanedString = Regex.Replace(contents, ",", " ");
+
+            dataDisplay.Text = CleanedString;
+
+
+
+
+        }
+        
+        public void SaveButton_Click(object sender, EventArgs e)
+        {
+            
+               
+            OpenFileDialog file = new OpenFileDialog(); // creates file dialog for user to pick a file to save to
+            file.ShowDialog(); // shows the file dialog
+            StreamWriter sw = new StreamWriter(file.FileName); // makes a streamwriter that will write to the new file
+            string contents = dataDisplay.Text; // retrieves the data that will be written into the file
+            sw.WriteLine(contents); // writes the data into the file
+            sw.Close();  
+        }
+
+        private void addDataButton_Click(object sender, EventArgs e)
+        {
+            string temp = dataInput.Text; // retrieves the user input that was entered in the input field so it can be added to file
+            contents = contents + temp + "\n\n"; // add the input data to the file data 
+            dataInput.Text = " "; // sets the input box to blank. Thought this would improve UX
+            dataDisplay.Text = contents; // puts the old data and new data into the data text box
+        }
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                int Start, End;
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+
+            return "";
+        }
+        struct classBlock
+        {
+            string className;
+            string profName;
+            string days;
+            int startTime;
+            int endTime;
+            public classBlock(int startTime, int endTime, string prof, string classname, string day)
+            {
+                this.startTime = startTime;
+                this.endTime = endTime;
+                this.days = day;
+                this.profName = prof;
+                this.className = classname;
+            }
+            public string getProf()
+            {
+                return this.profName;
+            }
+            public string getClassName()
+            {
+                return this.className;
+            }
+            public string getDays()
+            {
+                return this.days;
+            }
+            public int getStartTime()
+            {
+                return this.startTime;
+            }
+            public int getEndTime()
+            {
+                return this.endTime;
+            }
+
+
+        }
+        classBlock[] Class1 = new classBlock[200];
+        
+        private void processButton_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            int y = 0;
+            while(a < count)
+            {
+               string cleaned =  Regex.Replace(lines[a], ",", " ");
+                lines[a] = cleaned;
+                a = a + 1;
+            }
+
+            while(y < count)
+            {
+
+                string classNum = getBetween(lines[y], "CSC ", " ");
+                classNum = "CSC" + classNum;
+                string classProf = getBetween(lines[y], "\"", "\"");
+                string startTime = getBetween(lines[y], "[", " ");
+                string endTime = getBetween(lines[y], " ", "]");
+                int timeStart = Convert.ToInt32(startTime);
+                int timeEnd = Convert.ToInt32(endTime);
+                
+
+                Class1[y] = new classBlock(timeStart, timeEnd, classProf, classNum, "y");
+                y = y + 1;
+            }
+            //MessageBox.Show(lines[lines.Length -1]);
+            //MessageBox.Show(file.FileName);
+
+            
+
+
+
+
+            string output = Class1[4].getClassName() + " " + Class1[4].getProf() + " " + Class1[4].getStartTime() + " " + Class1[4].getEndTime();
+            
+            MessageBox.Show(output);
+            
+        }
+    }
+}
